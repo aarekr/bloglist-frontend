c3ba0469177690b27574e33f2b5b7f7f91a0310e
aarekr <aare212@hotmail.com>
2022-10-31 09:33:32 +0200

    Tehtävä 5.3 step3 ja 5.2 lisäykset

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newBlog, setNewBlog] = useState('')
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogURL, setNewBlogURL] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>setBlogs( blogs ))  
+    blogService.getAll().then(blogs => setBlogs( blogs ))  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -22,6 +33,8 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user)) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -38,6 +51,7 @@ const App = () => {
   const handleLogout = async (event) => {
     event.preventDefault()
     setUser(null)
+    window.localStorage.clear()
   }
 
   const loginForm = () => (
@@ -57,21 +71,44 @@ const App = () => {
 
   const blogForm = () => (
     <form onSubmit={addBlog}>
-      <input value={newBlog} onChange={handleBlogChange} />
-      <button type="submit">save</button>
+      <h2>create new</h2>
+      <div>title: <input value={newBlogTitle} onChange={handleBlogTitleChange} /></div>
+      <div>author: <input value={newBlogAuthor} onChange={handleBlogAuthorChange} /></div>
+      <div>url: <input value={newBlogURL} onChange={handleBlogURLChange} /></div>
+      <div><button type="submit">create</button></div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </form>
-
   )
 
   const addBlog = (event) => {
     event.preventDefault()
     console.log('adding a blog')
+    const newItem = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogURL,
+      likes: 0
+    }
+    blogService
+      .create(newItem)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+      })
+    setNewBlogTitle('')
+    setNewBlogAuthor('')
+    setNewBlogURL('')
   }
-  const handleBlogChange = () => {
-    console.log('handleBlogChange')
+
+  const handleBlogTitleChange = (event) => {
+    setNewBlogTitle(event.target.value)
+  }
+  const handleBlogAuthorChange = (event) => {
+    setNewBlogAuthor(event.target.value)
+  }
+  const handleBlogURLChange = (event) => {
+    setNewBlogURL(event.target.value)
   }
 
   return (
