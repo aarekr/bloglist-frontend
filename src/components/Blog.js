e1747f189933efc865bdc8c50bd9d2d14eb38864
aarekr <aare212@hotmail.com>
2022-11-01 11:41:45 +0200

    Tehtävä 5.7* step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-    <div>
-      {blog.title} {blog.author}
-    </div>  
-)
-  
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (showDetails === false) {
+    return(
+      <div>
+        {blog.title}
+        <button onClick={() => setShowDetails(true)}>view</button>
+      </div>
+    )
+  }
+  else if (showDetails === true){
+    return(
+      <div style={blogStyle}>
+        <div>{blog.title} <button onClick={() => setShowDetails(false)}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author} - {blog.user.name}</div>
+      </div>
+    )
+  }
+}
+
 export default Blog
